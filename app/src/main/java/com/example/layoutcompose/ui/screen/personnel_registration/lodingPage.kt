ca3f53181ca8007c527d6b6cffd94118bddb5e08
AbtinAtef_Fa <abtin35te@gmail.com>
2024-08-24 09:51:46 -0700

    update

---
+++ app/src/main/java/com/example/layoutcompose/ui/screen/personnel_registration/lodingPage.kt
@@ -0,0 +1,9 @@
+package com.example.layoutcompose.ui.screen.personnel_registration
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun (modifier : Modifier = Modifier) {
+	
+}
